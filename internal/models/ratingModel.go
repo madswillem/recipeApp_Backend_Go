d5cb8de64842cde19b250c2d66201c9fa5e723df
Mads <121515803+madswillem@users.noreply.github.com>
2024-10-21 23:00:26 +0200

    started to update to atomic operations next add avrg calculation within same transaction

--- internal/models/ratingModel.go
+++ internal/models/ratingModel.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -10,27 +11,27 @@ import (
 )
 
 type RatingStruct struct {
-	ID             string    `db:"id" json:"id"`
-    CreatedAt      time.Time `db:"created_at" json:"created_at"`
-    IngredientID   *string   `db:"ingredient_id" json:"ingredient_id,omitempty"`
-    RecipeID       *string   `db:"recipe_id" json:"recipe_id,omitempty"`
-    Overall        float64   `db:"overall" json:"overall"`
-    Mon            float64   `db:"mon" json:"mon"`
-    Tue            float64   `db:"tue" json:"tue"`
-    Wed            float64   `db:"wed" json:"wed"`
-    Thu            float64   `db:"thu" json:"thu"`
-    Fri            float64   `db:"fri" json:"fri"`
-    Sat            float64   `db:"sat" json:"sat"`
-    Sun            float64   `db:"sun" json:"sun"`
-    Win            float64   `db:"win" json:"win"`
-    Spr            float64   `db:"spr" json:"spr"`
-    Sum            float64   `db:"sum" json:"sum"`
-    Aut            float64   `db:"aut" json:"aut"`
-    ThirtyDegree   float64   `db:"thirtydegree" json:"thirtydegree"`
-    TwentyDegree   float64   `db:"twentiedegree" json:"twentiedegree"`
-    TenDegree      float64   `db:"tendegree" json:"tendegree"`
-    ZeroDegree     float64   `db:"zerodegree" json:"zerodegree"`
-    SubZeroDegree  float64   `db:"subzerodegree" json:"subzerodegree"`
+	ID            string    `db:"id" json:"id"`
+	CreatedAt     time.Time `db:"created_at" json:"created_at"`
+	IngredientID  *string   `db:"ingredient_id" json:"ingredient_id,omitempty"`
+	RecipeID      *string   `db:"recipe_id" json:"recipe_id,omitempty"`
+	Overall       float64   `db:"overall" json:"overall"`
+	Mon           float64   `db:"mon" json:"mon"`
+	Tue           float64   `db:"tue" json:"tue"`
+	Wed           float64   `db:"wed" json:"wed"`
+	Thu           float64   `db:"thu" json:"thu"`
+	Fri           float64   `db:"fri" json:"fri"`
+	Sat           float64   `db:"sat" json:"sat"`
+	Sun           float64   `db:"sun" json:"sun"`
+	Win           float64   `db:"win" json:"win"`
+	Spr           float64   `db:"spr" json:"spr"`
+	Sum           float64   `db:"sum" json:"sum"`
+	Aut           float64   `db:"aut" json:"aut"`
+	ThirtyDegree  float64   `db:"thirtydegree" json:"thirtydegree"`
+	TwentyDegree  float64   `db:"twentiedegree" json:"twentiedegree"`
+	TenDegree     float64   `db:"tendegree" json:"tendegree"`
+	ZeroDegree    float64   `db:"zerodegree" json:"zerodegree"`
+	SubZeroDegree float64   `db:"subzerodegree" json:"subzerodegree"`
 }
 
 func (rating *RatingStruct) DefaultRatingStruct(recipe_id *string, ingredient_id *string) {
@@ -56,84 +57,60 @@ func (rating *RatingStruct) DefaultRatingStruct(recipe_id *string, ingredient_id
 	rating.SubZeroDegree = 1000.0
 }
 
-func (rating *RatingStruct) Update(change int) *error_handler.APIError {
-
-	result := rating
+func UpdateRating(change int) (*error_handler.APIError, []string) {
 	data, err := tools.GetCurrentData()
 
 	if err != nil {
-		return error_handler.New("fatal internal error", http.StatusInternalServerError, err)
+		return error_handler.New("fatal internal error", http.StatusInternalServerError, err), nil
 	}
 
-	percentage := 10.0
+	factor := 1.1
+	var result []string
 
 	switch data.Day {
 	case "Mon":
-		result.Mon += tools.PercentageCalculator(result.Mon*float64(change), percentage)
+		result = append(result, fmt.Sprintf("mon = mon * %f", factor*float64(change)))
 	case "Tue":
-		result.Tue += tools.PercentageCalculator(result.Tue*float64(change), percentage)
+		result = append(result, fmt.Sprintf("tue = tue * %f", factor*float64(change)))
 	case "Wed":
-		result.Wed += tools.PercentageCalculator(result.Wed*float64(change), percentage)
+		result = append(result, fmt.Sprintf("wed = wed * %f", factor*float64(change)))
 	case "Thu":
-		result.Thu += tools.PercentageCalculator(result.Thu*float64(change), percentage)
+		result = append(result, fmt.Sprintf("thu = thu * %f", factor*float64(change)))
 	case "Fri":
-		result.Fri += tools.PercentageCalculator(result.Fri*float64(change), percentage)
+		result = append(result, fmt.Sprintf("fri = fri * %f", factor*float64(change)))
 	case "Sat":
-		result.Sat += tools.PercentageCalculator(result.Sat*float64(change), percentage)
+		result = append(result, fmt.Sprintf("sat = sat * %f", factor*float64(change)))
 	case "Sun":
-		result.Sun += tools.PercentageCalculator(result.Sun*float64(change), percentage)
+		result = append(result, fmt.Sprintf("sun = sun * %f", factor*float64(change)))
 	default:
-		return error_handler.New("fatal internal error", http.StatusInternalServerError, errors.New("fatal internal error"))
+		return error_handler.New("fatal internal error", http.StatusInternalServerError, errors.New("fatal internal error")), nil
 	}
 
 	switch data.Season {
 	case "Win":
-		result.Win += tools.PercentageCalculator(result.Win*float64(change), percentage)
+		result = append(result, fmt.Sprintf("win = win * %f", factor*float64(change)))
 	case "Spr":
-		result.Spr += tools.PercentageCalculator(result.Spr*float64(change), percentage)
+		result = append(result, fmt.Sprintf("spr = spr * %f", factor*float64(change)))
 	case "Sum":
-		result.Sum += tools.PercentageCalculator(result.Sum*float64(change), percentage)
+		result = append(result, fmt.Sprintf("sum = sum * %f", factor*float64(change)))
 	case "Aut":
-		result.Aut += tools.PercentageCalculator(result.Aut*float64(change), percentage)
+		result = append(result, fmt.Sprintf("aut = aut * %f", factor*float64(change)))
 	default:
-		return error_handler.New("fatal internal error", http.StatusInternalServerError, errors.New("fatal internal error"))
+		return error_handler.New("fatal internal error", http.StatusInternalServerError, errors.New("fatal internal error")), nil
 	}
 
 	switch data.Temp {
 	case "subzerodegree":
-		result.SubZeroDegree += tools.PercentageCalculator(result.SubZeroDegree*float64(change), percentage)
+		result = append(result, fmt.Sprintf("subzerodegree = subzerodegree * %f", factor*float64(change)))
 	case "zerodegree":
-		result.ZeroDegree += tools.PercentageCalculator(result.ZeroDegree*float64(change), percentage)
+		result = append(result, fmt.Sprintf("zerodegree = zerodegree * %f", factor*float64(change)))
 	case "tendegree":
-		result.TenDegree += tools.PercentageCalculator(result.TenDegree*float64(change), percentage)
+		result = append(result, fmt.Sprintf("tendegree = tendegree * %f", factor*float64(change)))
 	case "twentiedegree":
-		result.TwentyDegree += tools.PercentageCalculator(result.TwentyDegree*float64(change), percentage)
+		result = append(result, fmt.Sprintf("twentiedegree = twentiedegree * %f", factor*float64(change)))
 	case "thirtydegree":
-		result.ThirtyDegree += tools.PercentageCalculator(result.ThirtyDegree*float64(change), percentage)
+		result = append(result, fmt.Sprintf("thirtydegree = thirtydegree * %f", factor*float64(change)))
 	}
 
-	arr := []float64{
-		result.Mon,
-		result.Tue,
-		result.Wed,
-		result.Thu,
-		result.Fri,
-		result.Sat,
-		result.Sun,
-
-		result.Win,
-		result.Spr,
-		result.Sum,
-		result.Aut,
-
-		result.ThirtyDegree,
-		result.TwentyDegree,
-		result.TenDegree,
-		result.ZeroDegree,
-		result.SubZeroDegree,
-	}
-
-	result.Overall = tools.CalculateAverage(arr)
-
-	return nil
+	return nil, result
 }
